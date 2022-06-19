c1e45fa3c1a0c5439b77c5c8ead34a3379e06ddb
ronenius <ronenius4@gmail.com>
2022-06-19 22:51:12 +0300

    login and register pages.

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button LgnBtn =findViewById(R.id.LgnBtn);
+        Button RegBtn = findViewById(R.id.RegBtn);
+        RegBtn.setOnClickListener(v -> {
+           Intent i = new Intent(this, Register.class);
+           startActivity(i);
+        });
     }
+
+
 }
